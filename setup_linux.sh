319f4dea6741211447981ae70659dcebdafc7159
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-01-27 14:31:36 +0000

    feat: add setup_linux.sh

---
+++ setup_linux.sh
@@ -0,0 +1,9 @@
+# Vim
+sudo apt-get update
+sudo apt-get install vim
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Atuin
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
